Fix gae sign

mushroom_rl/rl_utils/value_functions.py
@@ -101,17 +101,14 @@ def compute_gae(V, s, ss, r, absorbing, last, gamma, lam):
         v = V(s)
         v_next = V(ss)
 
-        v_ep, v_next_ep, r_ep, absorbing_ep = split_episodes(last, v.squeeze(), v_next.squeeze(), r.float(), absorbing)
+        v_ep, v_next_ep, r_ep, absorbing_ep = split_episodes(last, v.squeeze(), v_next.squeeze(), r, absorbing)
         gen_adv_ep = torch.zeros_like(v_ep)
-        diff = r_ep - v_ep
-        v_next_discounted = (1 - absorbing_ep.int()) * gamma * v_next_ep
         for rev_k in range(v_ep.shape[-1]):
             k = v_ep.shape[-1] - rev_k - 1
             if rev_k == 0:
-                gen_adv_ep[..., k] = diff[..., k] + v_next_discounted[..., k]
+                gen_adv_ep[..., k] = r_ep[..., k] - v_ep[..., k] + (1 - absorbing_ep[..., k].int()) * gamma * v_next_ep[..., k]
             else:
-                last_adv = gamma * lam * gen_adv_ep[..., k + 1]
-                gen_adv_ep[..., k] = diff[..., k] + v_next_discounted[..., k] + last_adv
+                gen_adv_ep[..., k] = r_ep[..., k] - v_ep[..., k] + (1 - absorbing_ep[..., k].int()) * gamma * v_next_ep[..., k] + gamma * lam * gen_adv_ep[..., k + 1]
 
         gen_adv = unsplit_episodes(last, gen_adv_ep).unsqueeze(-1)
 

tests/rl_utils/test_value_functions.py
@@ -26,12 +26,11 @@ def test_compute_advantage_montecarlo():
             return q[:, None], adv[:, None]
         
     torch.manual_seed(42)
-    test_value_functions(compute_advantage_montecarlo, advantage_montecarlo, 0.99)
+    _value_functions_tester(compute_advantage_montecarlo, advantage_montecarlo, 0.99)
     
 def test_compute_gae():
     def gae(V, s, ss, r, absorbing, last, gamma, lam):
         with torch.no_grad():
-            r = r.float()
             v = V(s)
             v_next = V(ss)
             gen_adv = torch.empty_like(v)
@@ -42,20 +41,17 @@ def test_compute_gae():
                     if not absorbing[k]:
                         gen_adv[k] += gamma * v_next[k]
                 else:
-                    diff = r[k] - v[k]
-                    v_next_discounted = gamma * v_next[k]
-                    last_adv = gamma * lam * gen_adv[k + 1]
-                    gen_adv[k] = diff + v_next_discounted + last_adv
+                    gen_adv[k] = r[k] - v[k] + gamma * v_next[k] + gamma * lam * gen_adv[k + 1]
             return gen_adv + v, gen_adv
 
     torch.manual_seed(42)
-    test_value_functions(compute_gae, gae, 0.99, 0.95)
+    _value_functions_tester(compute_gae, gae, 0.99, 0.95)
         
-def test_value_functions(test_fun, correct_fun, *args):
+def _value_functions_tester(test_fun, correct_fun, *args):
     mdp = Segway()
     V = Regressor(TorchApproximator, input_shape=mdp.info.observation_space.shape, output_shape=(1,), network=Net, loss=torch.nn.MSELoss(), optimizer={'class': torch.optim.Adam, 'params': {'lr': 0.001}})
 
-    state, action, reward, next_state, absorbing, last = get_episodes(mdp, 10)
+    state, action, reward, next_state, absorbing, last = _get_episodes(mdp, 10)
     
     correct_v, correct_adv = correct_fun(V, state, next_state, reward, absorbing, last, *args)
     v, adv = test_fun(V, state, next_state, reward, absorbing, last, *args)
@@ -71,7 +67,7 @@ def test_value_functions(test_fun, correct_fun, *args):
     assert torch.allclose(v, correct_v)
     assert torch.allclose(adv, correct_adv)
 
-def get_episodes(mdp, n_episodes=100):
+def _get_episodes(mdp, n_episodes=100):
     mu = torch.tensor([6.31154476, 3.32346271, 0.49648221]).unsqueeze(0)
     
     approximator = Regressor(LinearApproximator,